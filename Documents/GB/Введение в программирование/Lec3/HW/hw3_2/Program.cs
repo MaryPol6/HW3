3eca52778229a88ba506bb4b8011ee98d4bcc3bd
Mary Polyntseva <m-polyntseva@mail.ru>
2023-01-15 16:53:16 +0700

    HW3

---
+++ Documents/GB/Введение в программирование/Lec3/HW/hw3_2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве
+using static System.Console;
+Clear();
+WriteLine("Введите координаты точки А ");
+int x1 = int.Parse(ReadLine());
+int y1 = int.Parse(ReadLine());
+int z1 = int.Parse(ReadLine());
+WriteLine("Введите координаты точки B ");
+int x2 = int.Parse(ReadLine());
+int y2 = int.Parse(ReadLine());
+int z2 = int.Parse(ReadLine());
+double result = Math.Sqrt(Math.Pow((x1-x2),2)+Math.Pow((y1-y2),2)+Math.Pow((z1-z2),2));
+WriteLine(result);
---
+++ Documents/GB/Введение в программирование/Lec3/HW/hw3_3/Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+using static System.Console;
+Clear();
+int x = int.Parse(ReadLine());
+int y = 1;
+
+while(y<=x)
+{
+    WriteLine(Math.Pow(y,3));
+    y+=1;
+}
